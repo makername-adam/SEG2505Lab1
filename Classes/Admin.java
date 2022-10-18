7c330e7d8edc67f11b264964668c296e59e6d08d
Anasbrf <91440022+Anasbrf@users.noreply.github.com>
2022-10-18 19:54:39 +0100

    Add files via upload

---
+++ Classes/Admin.java
@@ -0,0 +1,36 @@
+public class Admin extends Utilisateur 
+{
+  //Instance variables ************************************************
+
+  /**
+   * Contains C(artesian) or P(olar) to identify the type of
+   * coordinates that are being dealt with.
+   */
+  
+
+
+  
+  //Constructors ******************************************************
+
+  /**
+   * Constructs a coordinate object, with a type identifier.
+   */
+  public Admin(String Courriel, String Password)
+  {
+    super(Courriel,Passwoed);
+  }
+ 
+  
+  //Instance methods **************************************************
+ 
+ 
+  /**
+   * Returns information about the coordinates.
+   *
+   * @return A String containing information about the coordinates.
+   */
+  public String toString()
+    {
+    
+  }
+}
